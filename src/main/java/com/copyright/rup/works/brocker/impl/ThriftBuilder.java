e6b0fcfae1d07594726e3b325470195173d02f6a
Pavel Yakovlev <pavelny@gmail.com>
2012-04-13 10:16:37 +0300

    change main class path in client task

---
+++ src/main/java/com/copyright/rup/works/brocker/impl/ThriftBuilder.java
@@ -0,0 +1,13 @@
+package com.copyright.rup.works.brocker.impl;
+
+/**
+ * //TODO class description
+ * <p/>
+ * Copyright (C) 2012 copyright.com
+ * <p/>
+ * Date: 4/13/12
+ *
+ * @author Pavel_Yakovlev
+ */
+public class ThriftBuilder {
+}
